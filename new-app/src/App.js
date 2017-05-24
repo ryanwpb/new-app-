4b1a954fedb48847b57305d3f1d73c3b10165ef9
Ryan Graham <ryangrahamwpb@gmail.com>
2017-05-24 19:10:21 +0000

    Add class welcome.

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from "./components/Welcome"
 
 class App extends Component {
   render() {
@@ -13,9 +14,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Welcome/>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ new-app/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+class Welcome extends Component{
+  render(){
+    return(
+      <h2>Hello</h2>
+      )
+    
+  }
+  
+}
+
+export default Welcome;
